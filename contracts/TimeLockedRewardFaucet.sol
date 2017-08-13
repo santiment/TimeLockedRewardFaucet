48b3fded4be2b248c48965baf2fc3f49cf63668a
human <dip@dip-office.net>
2017-08-14 01:13:51 +0200

    [!] more bugfixes

--- contracts/TimeLockedRewardFaucet.sol
+++ contracts/TimeLockedRewardFaucet.sol
@@ -7,31 +7,32 @@ contract ERC20_Transferable {
 
 contract TimeLockedRewardFaucet {
 
+    // =========== CONFIG START ===========
     address constant public MULTISIG_OWNER = 0x0;
     address constant public TEAM_WALLET = 0xA0D8F33Ef9B44DaAE522531DD5E7252962b09207;
 
     ERC20_Transferable public token = ERC20_Transferable(0x7C5A0CE9267ED19B22F8cae653F198e3E8daf098);
     uint constant public LOCK_RELASE_TIME = 1499846591 + 90 days; //block.timestamp(4011221) == 1499846591
     uint constant public WITHDRAWAL_END_TIME = LOCK_RELASE_TIME + 30 days;
-    uint constant public TOKEN_AMOUNT_TO_DISTRIBUTE = 0x0;
-    uint constant public SHARES_NUM = 14;
+    // =========== CONFIG END ===========
 
     address[] public team_accounts;
     uint      public locked_since = 0;
+    uint      amount_to_distribute;
 
     function all_team_accounts() external constant returns(address[]) {
         return team_accounts;
     }
 
-    function timeToUnlockDDHHMM() external constant returns(uint[3]) {
+    function timeToUnlockDDHHMM() external constant returns(uint,uint,uint) {
         if (LOCK_RELASE_TIME > now) {
             uint diff = LOCK_RELASE_TIME - now;
             uint dd = diff / 1 days;
             uint hh = diff % 1 days / 1 hours;
             uint mm = diff % 1 hours / 1 minutes;
-            return [dd,hh,mm];
+            return (dd,hh,mm);
         } else {
-            return [uint(0), uint(0), uint(0)];
+            return (uint(0), uint(0), uint(0));
         }
     }
 
@@ -50,9 +51,11 @@ contract TimeLockedRewardFaucet {
             require(indexOf(team_accounts,msg.sender)==-1);
             team_accounts.push(msg.sender);
         } else if (state==State.WITHDRAWAL) {
+            // setup amount to distribute
+            if (amount_to_distribute==0) amount_to_distribute = token.balanceOf(this);
             //payout processing
             require(indexOf(team_accounts, msg.sender)>=0);
-            token.transfer(msg.sender,  TOKEN_AMOUNT_TO_DISTRIBUTE / SHARES_NUM);
+            token.transfer(msg.sender,  amount_to_distribute / team_accounts.length);
         } else if (state==State.CLOSED) {
             //collect unclaimed token to team wallet
             require(msg.sender == TEAM_WALLET);
